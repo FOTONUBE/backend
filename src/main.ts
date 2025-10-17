2e826087c2dbd323c402fd5c03a86f2801b94d07
Mauri <mauri.monzon91@gmail.com>
2025-10-17 01:05:42 -0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -16,6 +16,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
+      'http://localhost:3000',
       'https://frontend-two-ruddy-32.vercel.app',
       'https://fotonube.com',
     ],
